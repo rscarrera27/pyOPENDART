a6c714ea47f2cab954914986370d1d33800f5b4b
Seonghyeon Kim <self@seonghyeon.dev>
2021-01-10 21:45:32 +0900

    feat: Add company overview api

--- pyopendart/disclosure.py
+++ pyopendart/disclosure.py
@@ -107,6 +107,53 @@ class SearchResult:
         )
 
 
+@dataclass(frozen=True)
+class CompanyOverview:
+    @dataclass(frozen=True)
+    class Name:
+        kor: str
+        eng: str
+        stock: str
+
+    name: Name  # corp_name, corp_name_eng, stock_name
+    ticker: str  # stock_code
+    ceo_name: str  # ceo_nm
+    market: Market  # corp_cls
+    corporation_registration_number: str  # jurir_no
+    business_registration_number: str  # bizr_no
+    address: str  # adres
+    homepage_url: str  # hm_url
+    ir_url: str  # ir_url
+    phone_number: str  # phn_no
+    fax_number: str  # fax_no
+    industry_code: str  # induty_code
+    established_date: date  # est_dt
+    accounting_month: int  # acc_mt
+
+    @staticmethod
+    def from_dart_resp(resp):
+        return CompanyOverview(
+            name=CompanyOverview.Name(
+                kor=resp.get("corp_name"),
+                eng=resp.get("corp_name_eng"),
+                stock=resp.get("stock_name"),
+            ),
+            ticker=resp.get("stock_code"),
+            ceo_name=resp.get("ceo_nm"),
+            market=Market(resp.get("corp_cls")),
+            corporation_registration_number=resp.get("jurir_no"),
+            business_registration_number=resp.get("bizr_no"),
+            address=resp.get("adres"),
+            homepage_url=resp.get("hm_url"),
+            ir_url=resp.get("ir_url"),
+            phone_number=resp.get("phn_no"),
+            fax_number=resp.get("fax_no"),
+            industry_code=resp.get("induty_code"),
+            established_date=resp.get("est_dt"),
+            accounting_month=resp.get("acc_mt"),
+        )
+
+
 class PublicNotification:
     def __init__(self, api_key: str) -> None:
         self.client = DartClient(api_key)
@@ -138,3 +185,9 @@ class PublicNotification:
 
         return SearchResult.from_dart_resp(resp)
 
+    def company_overview(
+        self,
+        corporation_code: str,  # corp_code
+    ) -> CompanyOverview:
+        resp = self.client.json("company", corp_code=corporation_code)
+        return CompanyOverview.from_dart_resp(resp)
