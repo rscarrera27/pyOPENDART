3f18f0f826dbfb95fa1a8e91cd8a20fd43c24326
Seonghyeon Kim <self@seonghyeon.dev>
2021-01-17 18:03:49 +0900

    feat: Add get accounts of multiple corps

--- pyopendart/financial_information.py
+++ pyopendart/financial_information.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Tuple
+from typing import Dict, Sequence, Tuple
 
 from pyopendart.client import DartClient
 from pyopendart.common import ReportType, dart_atoi
@@ -15,6 +15,7 @@ class FinancialStatementType(Enum):
 class Account:
     receipt_no: str  # rcept_no
     business_year: int  # bsns_year
+    corporation_code: str  # corp_code
     ticker: str  # stock_code
     report_type: ReportType  # reprt_code
 
@@ -39,6 +40,7 @@ class Account:
         return Account(
             receipt_no=resp.get("rcept_no"),
             business_year=int(resp.get("bsns_year")),
+            corporation_code=resp.get("corp_code"),
             ticker=resp.get("stock_code"),
             report_type=ReportType(int(resp.get("reprt_code"))),
             title=resp.get("account_nm"),
@@ -68,16 +70,40 @@ class FinancialInformation:
         self.client = DartClient(api_key)
 
     def get_financial_statements_of_major_accounts(
-        self, corporation_code: str, business_year: int, report_type: ReportType
-    ) -> Tuple[Account]:
-        params = {"corp_code": corporation_code, "bsns_year": str(business_year), "reprt_code": report_type.value}
-        resp = self.client.json("fnlttSinglAcnt", **params)
+        self, corporation_code: Sequence[str], business_year: int, report_type: ReportType
+    ) -> Dict[str, Tuple[Account]]:
+        if len(corporation_code) == 0:
+            return {}
+        elif len(corporation_code) == 1:
+            params = {
+                "corp_code": corporation_code[0],
+                "bsns_year": str(business_year),
+                "reprt_code": report_type.value,
+            }
+            resp = self.client.json("fnlttSinglAcnt", **params)
 
-        accounts = [Account.from_dart_resp(i) for i in resp.get("list", [])]
-        accounts.sort(key=lambda v: v.order)
-        accounts = tuple(accounts)
+            accounts = [Account.from_dart_resp(i) for i in resp.get("list", [])]
+            accounts.sort(key=lambda v: v.order)
+            accounts = tuple(accounts)
 
-        return accounts
+            return {corporation_code[0]: accounts}
+        else:
+            params = {
+                "corp_code": ','.join(corporation_code),
+                "bsns_year": str(business_year),
+                "reprt_code": report_type.value,
+            }
+            resp = self.client.json("fnlttMultiAcnt", **params)
+
+            accounts = [Account.from_dart_resp(i) for i in resp.get("list", [])]
+            grouped_accounts = {}
+
+            for corp in corporation_code:
+                accounts_filtered_by_corp = [i for i in accounts if i.corporation_code == corp]
+                accounts_filtered_by_corp.sort(key=lambda v: v.order)
+                grouped_accounts[corp] = tuple(accounts_filtered_by_corp)
+
+            return grouped_accounts
 
     def get_all_financial_statements(self):
         pass
