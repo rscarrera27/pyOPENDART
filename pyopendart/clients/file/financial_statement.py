7c2598826805ca332cc1048a1b9a7c41ac0707e7
Seonghyeon Kim <self@seonghyeon.dev>
2021-02-13 17:09:45 +0900

    fix: Add save to param

--- pyopendart/clients/file/financial_statement.py
+++ pyopendart/clients/file/financial_statement.py
@@ -3,9 +3,9 @@ from pyopendart.enums import ReportType
 
 
 class FinancialStatementFileDownloader(ClientBase):
-    def get_xbrl_document(self, corporation_code: str, report_type: ReportType):
+    def get_xbrl_document(self, corporation_code: str, report_type: ReportType, save_to: str):
         params = {
             "corp_code": corporation_code,
             "reprt_code": report_type.value,
         }
-        self.client.download("fnlttXbrl", params)
+        self.client.download("fnlttXbrl", params, save_to=save_to)
